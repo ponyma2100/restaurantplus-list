ddeb5e97eba087cc1e15b4587f424164f99f65f3
Pony <ponyma2100@gmail.com>
2020-07-26 04:09:34 +0800

    feat: add mongoose

---
+++ models/restaurant.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  google_map: {
+    type: String,
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+  }
+})
+module.exports = mongoose.model('Restaurant', restaurantSchema)
