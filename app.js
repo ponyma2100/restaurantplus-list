147ae45eb44ac8e16e23de214cfc7d3ff86cd0b8
Pony <ponyma2100@gmail.com>
2020-07-26 16:43:20 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const Restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 const db = mongoose.connection
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -15,7 +21,10 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('restaurant list')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 app.listen(port, (req, res) => {
