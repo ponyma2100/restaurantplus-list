ae9131946648de01bf859609d0be5e94f16520c9
Pony <ponyma2100@gmail.com>
2020-07-26 17:19:11 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
 const app = express()
 const port = 3000
 const db = mongoose.connection
@@ -9,6 +11,7 @@ const db = mongoose.connection
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -24,6 +27,17 @@ app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// create new
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  return Restaurant.create({ name }) //存入資料庫
+    .then(() => res.redirect('/'))
     .catch(error => console.error(error))
 })
 
