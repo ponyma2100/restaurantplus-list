5196f009720c0b6590f887697f8294c90b330c46
Pony <ponyma2100@gmail.com>
2020-08-23 02:07:47 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 // 引入路由模組
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
+
 router.use('/restaurants', restaurants)
 
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
