5d2c03d018882f60fe33319e2f7874851b90bb88
Pony <ponyma2100@gmail.com>
2020-08-23 16:11:37 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override') //載入method-override
 const routes = require('./routes')
 const app = express()
 const port = 3000
+const flash = require('connect-flash')
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -25,9 +26,13 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated() //回傳布林值
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
